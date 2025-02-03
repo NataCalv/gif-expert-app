88f93600b9bf1628b69d62cfff9e7d8f07bbf695
Natalia <natacalv@emeal.nttdata.com>
2025-02-03 10:27:53 +0100

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=JOJBKEe2D8ylKDPNQmIa61F5BkPGaXgN&q=${ category }&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ category }&limit=10`
     const resp = await fetch( url );
     const { data } = await resp.json();
 
